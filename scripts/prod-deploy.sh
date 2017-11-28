dc586cadfb611e062037bece946dc9849fe09fa9
Chris Ramón <rchristian.ramon@gmail.com>
2017-11-28 14:49:36 -0500

    test

---
+++ scripts/prod-deploy.sh
@@ -0,0 +1,11 @@
+echo "tag: $0"
+echo $1
+git config --global user.email "rchristian.ramon@gmail.com"
+git config --global user.name "Travis-CI"
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+npm run build
+ls
+git checkout master
+git add . && git commit -m "$0" && git push origin master
+echo "abc"
